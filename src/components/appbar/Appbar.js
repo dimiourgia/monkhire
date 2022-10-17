a6c8e8286c2ed5e51bcf382ccaaa9df52b32df9f
ajay <ajayaxes318@gmail.com>
2022-10-17 21:35:18 +0530

    created empty components for homepage

---
+++ src/components/appbar/Appbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Appbar.css';
+
+function Appbar(){
+    return(
+        <div className="appbar dark-theme">
+            <div className="wrapper">
+
+            </div>
+        </div>
+    );
+}
+
+export default Appbar;
---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+import Appbar from '../appbar/Appbar';
+
+
+function Home(){
+    return(
+        <Appbar></Appbar>
+    );
+}
+
+
+export default Home;
